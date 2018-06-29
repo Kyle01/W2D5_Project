998269693e9c35d73632ebffc1f91af389ad0615
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-06-29 14:48:45 -0700

    completed phase 2 of myHashMap

--- MyHashMap/lib/p01_int_set.rb
+++ MyHashMap/lib/p01_int_set.rb
@@ -83,18 +83,36 @@ class ResizingIntSet
   end
   
   def insert(num)
+    unless include?(num)
+      resize! if num_buckets <= @count
+      @store[bucket_id(num)] << num 
+      @count += 1
+    end
   end
   
   def remove(num)
+    if include?(num)
+      self[bucket_id(num)].delete(num)
+      @count -= 1 
+    end
   end
   
   def include?(num)
+    self[bucket_id(num)].include?(num)
   end
   
   private
   
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num]
+  end
+  
+  def []=(num, value)
+    @store[num] = value
+  end
+  
+  def bucket_id(num)
+    num % num_buckets
   end
   
   def num_buckets
@@ -102,5 +120,17 @@ class ResizingIntSet
   end
   
   def resize!
+    new_array = Array.new(num_buckets*2) {Array.new}
+    
+    bucket_length = new_array.length
+    
+    @store.each do |arr|
+      arr.each do |el|
+        new_bucket_id = el % bucket_length
+        new_array[new_bucket_id] << el 
+      end
+    end
+    
+    @store = new_array
   end
 end
--- MyHashMap/lib/p02_hashing.rb
+++ MyHashMap/lib/p02_hashing.rb
@@ -4,18 +4,29 @@ end
 
 class Array
   def hash
+    return self.object_id.hash if self.empty?
+    self.reduce{ |el| el.hash} 
   end
 end
 
 class String
   def hash
-  end
+    alphabet = ("a".."z").to_a
+    seed = 0
+    self.split("").each_with_index {|c,i| seed += (alphabet.find_index(c.downcase) % (i+1))}
+    seed *= self.length
+    seed.hash
+    end
 end
 
 class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    seed = 0
+    self.each do |k,v|
+      seed += k.hash - v.hash
+    end
+    seed
   end
 end
