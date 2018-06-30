f57a348bb0b6832b3c2f7595a3409eda998d638d
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-06-29 17:03:53 -0700

    Almost done phase 5

--- MyHashMap/lib/p05_hash_map.rb
+++ MyHashMap/lib/p05_hash_map.rb
@@ -1,52 +1,65 @@
 require_relative 'p04_linked_list'
 
 class HashMap
+  include Enumerable
   attr_reader :count
-
+  
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { LinkedList.new }
     @count = 0
   end
-
+  
   def include?(key)
-    @store[bucket(key)].include?(key.hash)
+    @store[bucket(key)].include?(key)
   end
-
+  
   def set(key, val)
+    if include?(key)
+      @store[bucket(key)].update(key, val)
+    else
+      @store[bucket(key)].append(key, val)
+      @count += 1
+    end
   end
-
+  
   def get(key)
     @store[bucket(key)].get(key)
   end
-
+  
   def delete(key)
-  end
-
-  def each
+    @store[bucket(key)].remove(key)
+    @count -= 1
   end
   
+  def each(&prc)
+    @store.each do |lst|
+      lst.each do |node|
+        k = node.key
+        v = node.val
+        prc.call(k,v)
+      end
+    end
+  end
+
+  def to_s
+    pairs = inject([]) do |strs, (k, v)|
+      strs << "#{k.to_s} => #{v.to_s}"
+    end
+    "{\n" + pairs.join(",\n") + "\n}"
+  end
   
-
-  # uncomment when you have Enumerable included
-  # def to_s
-  #   pairs = inject([]) do |strs, (k, v)|
-  #     strs << "#{k.to_s} => #{v.to_s}"
-  #   end
-  #   "{\n" + pairs.join(",\n") + "\n}"
-  # end
-
   alias_method :[], :get
   alias_method :[]=, :set
-
+  
   private
-
+  
   def num_buckets
     @store.length
   end
-
+  
   def resize!
   end
-
+  
   def bucket(key)
     key.hash % num_buckets
   end
